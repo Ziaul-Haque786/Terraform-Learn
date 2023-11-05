e08573e2a81e6ba4da51454ccff202db07d65048
Ziaul-Haque786 <85923814+Ziaul-Haque786@users.noreply.github.com>
2023-11-05 11:45:06 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -7,15 +7,7 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  # Configuration options
-  subscription_id = "7d8c9d23-c48a-4064-9fc4-2ce2aa23f097"
-  tenant_id = "597cd869-12dc-41d2-ad26-7970155104d6"
-  client_id = "2b296b36-b904-49dc-b59d-dffb7e1b8558"
-  client_secret = "mtc8Q~dzAdU.Y_uiQCkJ66dXIZpBzg4gQVFhLbe2"
-  features {}
 
-}
 
 resource "azurerm_resource_group" "appgrp" {
   name     = "app-grp"
