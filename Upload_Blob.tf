5730436826a348f5adc923070bd1c1b758620043
MD Haque <ziacs2015@gmail.com>
2023-11-06 12:16:48 +0530

    Uploading Blob

---
+++ Upload_Blob.tf
@@ -0,0 +1,15 @@
+resource "azurerm_storage_container" "container1" {
+  name                  = "container1"
+  storage_account_name  = "appstr1122"
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_blob" "blobfile" {
+  name                   = "main.tf"
+  storage_account_name   = "appstr1122"
+  storage_container_name = "container1"
+  type                   = "Block"
+  source                 = "main.tf"
+
+  depends_on = [ azurerm_storage_container.container1 ]
+}
