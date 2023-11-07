890dc1edd09e07f5d78861abc78955974a17efe1
MD Haque <ziacs2015@gmail.com>
2023-11-07 18:42:04 +0530

    Adding Blob

--- Upload_Blob.tf
+++ Upload_Blob.tf
@@ -1,17 +1,17 @@
 resource "azurerm_storage_container" "container1" {
   name                  = "container1"
-  storage_account_name  = "appstr1122"
+  storage_account_name  = "appstr11212"
   container_access_type = "blob"
 
-  depends_on = [ azurerm_storage_account.appstr1122 ]
+  depends_on = [ azurerm_storage_account.appstr11212 ]
 }
 
 resource "azurerm_storage_blob" "blobfile" {
   name                   = "main.tf"
-  storage_account_name   = "appstr1122"
+  storage_account_name   = "appstr11212"
   storage_container_name = "container1"
   type                   = "Block"
-  source                 = "main.tf"
+  source                 = "C:/Terraform_Project/All Resources/Terraform-Learn/main.tf"
 
   depends_on = [ azurerm_storage_container.container1 ]
 }
