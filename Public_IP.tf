71f77f6596086141adc12c9887a8d9184aa5d1d5
MD Haque <ziacs2015@gmail.com>
2023-11-07 18:40:33 +0530

    Creating Public IP

---
+++ Public_IP.tf
@@ -0,0 +1,8 @@
+resource "azurerm_public_ip" "appip" {
+  name                = "app-ip"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  allocation_method   = "Static"
+
+  depends_on = [ azurerm_resource_group.appgrp ]
+}
